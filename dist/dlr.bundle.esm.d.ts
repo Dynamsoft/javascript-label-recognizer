f82bf1868971a2e737ee2d589a2b4d8ec31d8002
DMGithubPublisher <github@dynamsoft.com>
2024-05-20 15:11:02 +0800

    3.2.3000

---
+++ dist/dlr.bundle.esm.d.ts
@@ -0,0 +1,6 @@
+export * from 'dynamsoft-core';
+export * from 'dynamsoft-license';
+export * from 'dynamsoft-capture-vision-router';
+export * from 'dynamsoft-camera-enhancer';
+export * from 'dynamsoft-label-recognizer';
+export * from 'dynamsoft-utility';
